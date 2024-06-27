5d0846243ddcca075d4a52fc50f150d34cd88d00
MickaelTherrien <mickael_therrien@hotmail.com>
2024-06-27 14:27:13 -0400

    Astres 2

--- src/Soleil.java
+++ src/Soleil.java
@@ -1,5 +1,10 @@
 public class Soleil extends Astre{
 
+    /**
+     *
+     * @param nom
+     * @param masse
+     */
     public Soleil(String nom, double masse) {
         super(nom, masse);
     }
